9fd0bed83408bf14dbedf26ed62b2f5a28ce6dd0
efim0901 <efimovich.w@gmail.com>
2025-01-29 21:47:24 +0300

    Add files via upload

--- send_form.php
+++ send_form.php
@@ -6,7 +6,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     
     // Проверьте, что все поля заполнены
     if (!empty($name) && !empty($email) && !empty($message)) {
-        $to = 'efimovich.w@gmail.com'; // Замените на ваш email
+        $to = 'your-email@example.com'; // Замените на ваш email
         $subject = 'Новое сообщение с сайта БелВторПереработка';
         $body = "Имя: $name\nEmail: $email\nСообщение:\n$message";
         $headers = "From: $email";
